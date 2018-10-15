628bb08c05ef43f9f5c6961cab5e317e225c066a
albakag <Submarine1986>
2018-10-15 09:57:05 +0300

    1) Сохранение состояний с помощью методов saveInstanceState() и restoreInstanceState(); 2) Запрет на пересоздание activity для страницы workoutListActivity путем запрета переворота экрана в манифесте. 3) Выход из приложения по нажатию кнопки Exit в выпадающем списке меню. 4) Прописывание lifecycle activity.

--- app/src/main/java/com/example/dude/workoutproject/Activity/WorkoutListActivity.java
+++ app/src/main/java/com/example/dude/workoutproject/Activity/WorkoutListActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +18,7 @@ public class WorkoutListActivity extends AppCompatActivity {
     Button pushUpButton;
     Button squitting;
     Button call;
+    final String TAG = "WorkoutListActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,6 +26,42 @@ public class WorkoutListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_workout_list);
 
         initGUI();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "Вызван метод onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Вызван метод onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Вызван метод onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Вызван метод onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "Вызван метод onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Вызван метод onDestroy");
     }
 
     private void initGUI() {
